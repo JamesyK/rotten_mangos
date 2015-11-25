74fea08168730641a3effed3cd4b39be47619ea0
JamesyK <jameskanegae@gmail.com>
2015-11-25 04:36:15 +0000

    added admin user view

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     resources :users
   end
 
+  match '/swap_account/:id' => 'admin/users#swap_account', via: 'GET'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
